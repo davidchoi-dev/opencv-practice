4c911cc3c3ad3d81ac7f80e1bf9777d45ea03a43
nicewoong <thoughtscode@gmail.com>
2018-01-05 18:44:31 +0900

    Create ocr.py to practice OCR

---
+++ ocr.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+""" This is a practice code to learn the concept of OCR
+"""
+__author__ = "Wooongje Han"
+
+from PIL import Image
+from pytesseract import *
+
+def orc_test():
+
+
+
+def main():
+    orc_test()
+    return None
+
+
+if __name__ == "__main__":
+    main()
