bdc516bd340d581da2308731537eb6439deb90ef
nicewoong <thoughtscode@gmail.com>
2018-01-05 11:19:15 +0900

    Create the main.py to write code

---
+++ main.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+""" This is a practice code to learn the concept of OpenCV
+while performing the tutorial with python.
+"""
+__author__ = "Wooongje Han"
+
+import cv2  # import openCV-Python pacage
+import numpy as np
+import matplotlib
+
+
+def main():
+
+    return None
+
+
+if __name__ == "__main__":
+    main()
