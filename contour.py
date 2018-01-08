effb97f705158870f4cb9c7514964ac3a513c21d
nicewoong <thoughtscode@gmail.com>
2018-01-08 17:12:49 +0900

    Add rectangle_contour()

--- contour.py
+++ contour.py
@@ -23,7 +23,6 @@ def thresholding_adaptive():
     cv2.destroyAllWindows()  # 이미지 윈도우 닫기
 
 
-
 def draw_contour():
     filename = 'C:/Users/viva/PycharmProjects/opencv-practice/images/ad_text2.jpg'
     image_origin = cv2.imread(filename)
@@ -58,6 +57,7 @@ def draw_contour():
 
 
 
+
 def approx_contour():
     filename = 'C:/Users/viva/PycharmProjects/opencv-practice/images/bbox.png'
     image_origin = cv2.imread(filename)
@@ -88,8 +88,31 @@ def approx_contour():
     cv2.imwrite('C:/Users/viva/PycharmProjects/opencv-practice/images/bbox_result.png', image_origin)
 
 
+def rectangle_contour():
+    filename = 'C:/Users/viva/PycharmProjects/opencv-practice/images/ad_text2.jpg'
+    image_origin = cv2.imread(filename)
+    image_copy_grey = cv2.cvtColor(image_origin, cv2.COLOR_BGR2GRAY)  # grey scale 로 복사합니다.
+
+    block_size = 15  # 픽셀에 적용할 threshold값을 계산하기 위한 블럭 크기. 적용될 픽셀이 블럭의 중심이 됨. 따라서 blocksize 는 홀수여야 함
+    subtract_val = 2  # 보정 상수
+    adaptive_gaussian_image = cv2.adaptiveThreshold(image_copy_grey, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                                    cv2.THRESH_BINARY,  block_size, subtract_val)
+
+    # =========== find contour ===========
+    # contours는 point의 list형태.
+    # hierarchy는 contours line의 계층 구조
+    # Threshold 적용한 이미지에서 contour 들을 찾아서 contours 변수에 저장하기
+    _, contours, _ = cv2.findContours(adaptive_gaussian_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)  # 좌상단 꼭지점 좌표 , width, height
+        cv2.rectangle(image_origin, (x, y), (x+w, y+h), (0, 255, 0), 2)  # 원본 이미지 위에 사각형 그리기!
+
+    cv2.imwrite('C:/Users/viva/PycharmProjects/opencv-practice/images/ad_text2_rectangle_result.png', image_origin)
+
+
 def main():
-    approx_contour()
+    rectangle_contour()
     return None
 
 
